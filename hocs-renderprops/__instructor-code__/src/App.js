7eb1b8e8de31e23e55045374ac178014f620a43c
spiltmiruku <spiltmiruku@gmail.com>
2019-12-15 22:25:23 -0700

    Followed instructions to step 6

---
+++ hocs-renderprops/__instructor-code__/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+import Hooks from './Components/Hooks';
+import RenderProp from './Components/RenderProp';
+import HOC from './Components/HOC';
+
+import Toggle from './Components/Toggle';
+
+const artists = [
+  {
+      name: 'Modest Mouse',
+      genre: 'Alternative',
+      artistImg:
+          'https://www.kansascity.com/latest-news/by7r7a/picture210718134/ALTERNATES/FREE_1140/modest%20mouse.jpg'
+  },
+  {
+      name: 'Chance the Rapper',
+      genre: 'Rap',
+      artistImg:
+          'http://s3.amazonaws.com/hiphopdx-production/2016/12/Chance-The-Rapper-by-Mike-Lavin-@thehomelesspimp-4-1-800x600.jpg'
+  },
+  {
+      name: 'Talking Heads',
+      genre: 'Alternative',
+      artistImg:
+          'https://www.desktopbackground.org/download/800x600/2015/04/29/940579_talking-heads-this-must-be-the-place-naive-melody-youtube_1920x1080_h.jpg'
+  },
+  {
+      name: 'Atmosphere',
+      genre: 'Rap',
+      artistImg: 'https://storage.googleapis.com/rhymesayers/images/_tile/atmosp.png'
+  },
+  {
+      name: 'David Bowie',
+      genre: 'Alternative',
+      artistImg:
+          'https://media.npr.org/assets/img/2016/01/11/gettyimages-114939559-152a6dea2f0ae78878fb4b1d2a596cb229556a44-s800-c85.jpg'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hooks</h1>
+        <Hooks />
+        <h1>Higher Order Component</h1>
+        <Toggle render = {() => (
+        <>
+          <HOC list={artists} genre=''/>
+          <HOC list={artists} genre='Rap'/>
+          <HOC list={artists} genre='Alternative'/>
+        </>
+        )} />
+        <h1>Render Props</h1>
+        <RenderProp render={(props) => props('Rap', artists)} />
+        <RenderProp render={(props) => props('Alternative', artists)} />
+        <RenderProp render={(props) => props('', artists)} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ hocs-renderprops/__instructor-code__/src/Components/HOC.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const withFilteredProps = (BaseComponent) => ({list, genre}) => {
+    const filteredList = genre ? list.filter((artist) => artist.genre === genre) : list
+    return <BaseComponent artistList={filteredList}/>
+}
+
+const DisplayList = ({artistList}) => {
+    return (
+        <div>
+            {artistList.map((artist) => (
+                <div className='artist-container' key={artist.name}>
+                    <img className='artist-img' src={artist.artistImg} alt={artist.name} />
+                    <div className='artist-info'>
+                        <h3>{artist.name}</h3>
+                        <p>{artist.genre}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+const HOC = withFilteredProps(DisplayList);
+
+export default HOC;
